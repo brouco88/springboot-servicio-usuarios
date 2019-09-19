43d83f5f6addce381676b2162b7ba55692198926
Brouco <brouco@optaresolutions.com>
2019-09-19 12:24:04 +0200

    Se sube el DAO modificado

--- src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -1,5 +1,6 @@
 package com.formacionbdi.springboot.app.usuarios.models.dao;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -14,5 +15,6 @@ public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
 	@RestResource(path="buscar-username")
 	public Usuario findByUsername(@Param("username")String username);
 	
+	@Query(value="select u from Usuario u where u.username=?1")
 	public Usuario obtenerPorUsername(String username);
 }
