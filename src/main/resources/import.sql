26d9c5ccfda841dadfce97b08e888e5f1b84aded
Brouco <brouco@optaresolutions.com>
2019-09-19 12:20:54 +0200

    Modificado import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert into usuarios (username,password,enabled,nombre,apellido,email) values ('brouco','12345',1,'basilio','rouco','brouco@gmail.com');
-insert into usuarios (username,password,enabled,nombre,apellido,email) values ('jdoe','12345',1,'john','doe','jdoe@gmail.com');
+insert into usuarios (username,password,enabled,nombre,apellido,email) values ('brouco','$2a$10$WWJ8srkV.Oy1OktZai2bEe8hXRhuWUG8jgU9GwgTAlmpzFui.AyBS',1,'basilio','rouco','brouco@gmail.com');
+insert into usuarios (username,password,enabled,nombre,apellido,email) values ('jdoe','$2a$10$nJqQdaKii7HD3IkFTp2F8eBhlBWRCis/yeaTQ38yTlexWi.9wW5au',1,'john','doe','jdoe@gmail.com');
 
 insert into roles (nombre) values ('ROLE_USER');
 insert into roles (nombre) values ('ROLE_ADMIN');
